443cf7ac2f071feea22e1d7bbf05e307e34f379b
Sulimenko Maxim <sulimenko.maxim@gmail.com>
2024-12-05 09:20:12 +0300

    add new fields to models

--- server/models/models.go
+++ server/models/models.go
@@ -19,37 +19,42 @@ type User struct {
 }
 
 type Payment struct {
-	ID            primitive.ObjectID `bson:"_id,omitempty"`
-	Price         string             `bson:"price"`
-	PaymentMethod string             `bson:"payment_method"`
-	PaymentStatus string             `bson:"payment_status"`
-	CreatedAt     time.Time          `bson:"created_at"`
-	UpdatedAt     time.Time          `bson:"updated_at"`
+	ID            primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Price         string             `bson:"price" json:"price"`
+	PaymentMethod string             `bson:"payment_method" json:"payment_method"`
+	PaymentStatus string             `bson:"payment_status" json:"payment_status"`
+	CreatedAt     time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt     time.Time          `bson:"updated_at" json:"updated_at"`
+	JobID         primitive.ObjectID `bson:"job_id" json:"job_id"`
 }
 
 type Job struct {
-	ID             primitive.ObjectID `bson:"_id,omitempty"`
-	UserID         primitive.ObjectID `bson:"user_id"`
-	Title          string             `bson:"title"`
-	Status         string             `bson:"status"`
-	SourceLanguage string             `bson:"source_language"`
-	FileFormat     string             `bson:"file_format"`
-	Description    string             `bson:"description"`
-	InputFile      string             `bson:"input_file"`
-	OutputFile     string             `bson:"output_file"`
-	CreatedAt      time.Time          `bson:"created_at"`
-	UpdatedAt      time.Time          `bson:"updated_at"`
-	HostID         primitive.ObjectID `bson:"host_id"`
+	ID                      primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	UserID                  primitive.ObjectID `bson:"user_id" json:"user_id"`
+	Title                   string             `bson:"title" json:"title"`
+	Status                  string             `bson:"status" json:"status"`
+	SourceLanguage          string             `bson:"source_language" json:"source_language"`
+	FileFormat              string             `bson:"file_format" json:"file_format"`
+	Description             string             `bson:"description" json:"description"`
+	InputFile               string             `bson:"input_file" json:"input_file"`
+	OutputFile              string             `bson:"output_file" json:"output_file"`
+	CreatedAt               time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt               time.Time          `bson:"updated_at" json:"updated_at"`
+	EstimatedFinishDatetime time.Time          `bson:"estimated_finish_datetime" json:"estimated_finish_datetime"`
+	HostID                  primitive.ObjectID `bson:"host_id" json:"host_id"`
 }
 
 type Server struct {
-	ID             primitive.ObjectID   `bson:"_id,omitempty"`
-	Hostname       string               `bson:"hostname"`
-	Address        string               `bson:"address"`
-	Description    string               `bson:"description"`
-	Status         string               `bson:"status"`
-	CreatedAt      time.Time            `bson:"created_at"`
-	UpdatedAt      time.Time            `bson:"updated_at"`
-	CurrentTasks   []primitive.ObjectID `bson:"current_tasks"`
-	CompletedTasks []primitive.ObjectID `bson:"completed_tasks"`
+	ID             primitive.ObjectID   `bson:"_id,omitempty" json:"id,omitempty"`
+	Hostname       string               `bson:"hostname" json:"hostname"`
+	Address        string               `bson:"address" json:"address"`
+	Description    string               `bson:"description" json:"description"`
+	Status         string               `bson:"status" json:"status"`
+	CreatedAt      time.Time            `bson:"created_at" json:"created_at"`
+	UpdatedAt      time.Time            `bson:"updated_at" json:"updated_at"`
+	CurrentTasks   []primitive.ObjectID `bson:"current_tasks" json:"current_tasks"`
+	CompletedTasks []primitive.ObjectID `bson:"completed_tasks" json:"completed_tasks"`
+	CPUInfo        string               `bson:"cpu_info" json:"cpu_info"`
+	GPUInfo        string               `bson:"gpu_info" json:"gpu_info"`
+	RAMSizeGB      int32                `bson:"ram_size_gb" json:"ram_size_gb"`
 }
