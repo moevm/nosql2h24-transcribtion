e1334ddd518b502ac490b34c1f5856abae16a94f
ElizabethOnTheBathroomFloor <danil-pavlovz@mail.ru>
2024-12-19 15:13:33 +0300

    Добавил роут на вью пользовательской задачи

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue';
 import RegisterView from '../views/RegisterView.vue';
 import LoginView from '../views/LoginView.vue';
 import UserPanelView from '../views/UserPanelView.vue';
-
+import UserJobsView from '../views/UserJobsView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,6 +24,7 @@ const router = createRouter({
     { path: '/register', name: 'Register', component: RegisterView },
     { path: '/login', name: 'Login', component: LoginView },
     { path: '/user-panel', name: 'UserPanel', component: UserPanelView },
+    { path: '/user/:id/jobs', name: 'UserJobs', component: UserJobsView },
   ],
 })
 
