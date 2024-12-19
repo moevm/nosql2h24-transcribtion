c763a25ad211d7a9792247a77a16254bba68e63d
ElizabethOnTheBathroomFloor <danil-pavlovz@mail.ru>
2024-12-19 20:58:17 +0300

    изменил хоум вью

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,23 +1,26 @@
 <template>
   <div>
     <h1>Welcome to the App</h1>
-    <router-link v-if="!isAuthenticated" to="/register">Register</router-link>
-    <router-link v-if="!isAuthenticated" to="/login">Login</router-link>
-    <router-link v-if="isAuthenticated" to="/user-panel">User Panel</router-link>
+    <router-link v-if="!this.userStore.id" to="/register">Register</router-link>
+    <router-link v-if="!this.userStore.id" to="/login">Login</router-link>
+    <router-link v-if="this.userStore.id" to="/user-panel">User Panel</router-link>
   </div>
 </template>
 
 <script>
 import { computed } from 'vue';
 import { useStore } from 'vuex';
+import {useUserStore} from '../store/user';
 
 export default {
   setup() {
     const store = useStore();
+    const userStore = useUserStore();
     const isAuthenticated = computed(() => store.getters.isAuthenticated);
 
     return {
       isAuthenticated,
+      userStore
     };
   },
 };
