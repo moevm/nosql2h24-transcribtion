9d0a2e930830471e3ad2467229617029de08ccc8
ElizabethOnTheBathroomFloor <danil-pavlovz@mail.ru>
2024-12-19 20:57:04 +0300

    связал форму редактирования профиля с беком

--- frontend/src/components/EditProfileForm.vue
+++ frontend/src/components/EditProfileForm.vue
@@ -4,22 +4,22 @@
 
     <div>
       <label>Username:</label>
-      <input type="text" v-model="username" required />
+      <input type="text" v-model="username" />
     </div>
 
     <div>
       <label>Email:</label>
-      <input type="email" v-model="email" required />
+      <input type="email" v-model="email" />
     </div>
 
     <div>
       <label>Password:</label>
-      <input type="password" v-model="password" required />
+      <input type="password" v-model="password" />
     </div>
 
     <div>
       <label>Confirm Password:</label>
-      <input type="password" v-model="confirmPassword" required />
+      <input type="password" v-model="confirmPassword" />
     </div>
 
     <button type="submit">Save Changes</button>
@@ -30,14 +30,18 @@
 
 <script>
 import { ref, defineProps } from 'vue';
+import {useUserStore} from '../store/user';
+import { updateUser } from '../api/userApi';
 
 export default {
   props: {
     userData: Object,
   },
-  setup(props, { emit }) {
-    const username = ref(props.userData.username);
-    const email = ref(props.userData.email);
+  setup() {
+    const userStore = useUserStore();
+
+    const username = ref('')
+    const email = ref('');
     const password = ref('');
     const confirmPassword = ref('');
     const errorMessage = ref('');
@@ -49,18 +53,27 @@ export default {
       }
 
       errorMessage.value = '';
-      console.log({
-        username: username.value,
-        email: email.value,
-        password: password.value,
-      });
+
+      const payload = {
+        username: username.value ? username.value : userStore.username,
+        email: email.value ? email.value : userStore.email,
+        password_hash: password.value ? password.value : userStore.password_hash,
+        permissions: 'user'
+      };
+      
 
       // Emit the updated data to the parent component
-      emit('save', {
-        username: username.value,
-        email: email.value,
-        password: password.value,
-      });
+      try {
+        updateUser(userStore.id, payload)
+
+        userStore.username = payload.username;
+        userStore.email = payload.email;
+        userStore.password_hash = payload.password_hash;
+      } catch (error) {
+        errorMessage.value = 'Failed to update profile';
+      }
+
+
     };
 
     return { username, email, password, confirmPassword, handleEditProfile, errorMessage };
@@ -74,6 +87,7 @@ form {
   flex-direction: column;
   max-width: 300px;
   margin: auto;
+  background-color: rgb(87, 57, 101);
 }
 
 div {
